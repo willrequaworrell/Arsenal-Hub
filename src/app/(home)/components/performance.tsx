5259231b36c71e36a2caca8d3f68ee60b913553a
Will Worrell <willrequaworrell@gmail.com>
2025-10-20 00:17:04 -0400

    change performance widget to flex-col on mobile

--- src/app/(home)/components/performance.tsx
+++ src/app/(home)/components/performance.tsx
@@ -22,7 +22,7 @@ const Performance = async () => {
 
   return (
     <CardContainer title="Performance">
-      <div className="flex h-full items-center justify-around">
+      <div className="flex h-full flex-col items-center justify-center gap-y-3 sm:flex-row sm:justify-around sm:gap-y-0">
         <div className="text-center">
           <p className="font-bold text-[clamp(1rem,2vw,2rem)]">
             {`${record.w}-${record.d}-${record.l}`}
@@ -30,7 +30,8 @@ const Performance = async () => {
           <p className="text-xs text-slate-600 sm:text-sm">Record</p>
         </div>
 
-        <div className="h-2/3 w-[1px] bg-slate-300" />
+        {/* Divider - horizontal on mobile, vertical on larger screens */}
+        <div className="h-[1px] w-2/3 bg-slate-300 sm:h-2/3 sm:w-[1px]" />
         
         <div className="text-center">
           <p className="font-bold text-[clamp(1rem,2vw,2rem)]">
